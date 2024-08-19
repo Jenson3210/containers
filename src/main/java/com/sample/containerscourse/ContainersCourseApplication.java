cfa873b5665aa9e61a71bf18bca920ce7afb545e
Jente Sondervorst <jente.sondervorst@colruytgroup.com>
2024-08-19 14:04:35 +0200

    Initial commit from start.spring.io

---
+++ src/main/java/com/sample/containerscourse/ContainersCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sample.containerscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainersCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainersCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/containerscourse/ContainersCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.containerscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainersCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
