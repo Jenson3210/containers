f7f90bd65a8d3df75280323540431261c53aa42e
Jente Sondervorst <jente.sondervorst@colruytgroup.com>
2024-08-26 14:03:40 +0200

    Introduction to configMapGenerator

--- src/main/java/com/sample/containerscourse/domain/intro/HelloWorldController.java
+++ src/main/java/com/sample/containerscourse/domain/intro/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.sample.containerscourse.domain.intro;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloWorldController {
 
+    private final Environment environment;
+
+    public HelloWorldController(final Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping
     public String hello() {
-        return "Hello, World!";
+        return """
+               Hi there!
+               I'm running with %s profile(s) on port %s!
+               
+               The custom argument is: "%s"
+               """.formatted(String.join(", ", environment.getActiveProfiles()),
+                             environment.getProperty("local.server.port"),
+                             environment.getProperty("custom.argument"));
     }
 }
